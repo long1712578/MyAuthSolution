cc6ad2f165552922549c9a558c7071bcec5a42e0
PHẠM ĐÌNH LONG <1712578@student.hcmus.edu.vn>
2025-05-07 20:46:28 +0700

    Thêm comment và edit readme.md

--- OAuthOpenIdSolution/OAuthOpenIdSolution/Program.cs
+++ OAuthOpenIdSolution/OAuthOpenIdSolution/Program.cs
@@ -1,15 +1,16 @@
-using AuthServer;
+﻿using AuthServer;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddIdentityServer(options =>
 {
+    /// Dùng để Identity tự động thêm claim audience vào token dưới dạng chuỗi tĩnh.
     options.EmitStaticAudienceClaim = true;
 })
-.AddInMemoryClients(Config.Clients)
-.AddInMemoryApiScopes(Config.ApiScopes)
-.AddInMemoryIdentityResources(Config.IdentityResources)
-.AddDeveloperSigningCredential();
+.AddInMemoryClients(Config.Clients) // Khai báo client (app) nào được xin token.
+.AddInMemoryApiScopes(Config.ApiScopes) // Xác định quyền truy cập vào API.
+.AddInMemoryIdentityResources(Config.IdentityResources) // Nếu user login -> sẽ cho biết cấp phát infomation gì.
+.AddDeveloperSigningCredential(); // Dùng để ký token. Chỉ dùng cho dev, không dùng cho production.
 
 var app = builder.Build();
 app.UseIdentityServer();
---
+++ README.md
@@ -0,0 +1,14 @@
+﻿1. OAuth2 Client Credential flow giữa client và server
+2. Cấu hình API scope , clients trong IdentityServer (file Config.cs)
+3. Xác thực acess token trong API bằng AddJWtBearer()
+4. Github actions:
+	- Tự động build mỗi lần push
+	- Kiểm tra thành công của các project.
+5. Giải thích "Key tạm thời"
+	- Khi dùng AddDeveloperSigningCredential().
+		+ Tự tạo một cặp khóa RSA(private + public key).
+		+ Lưu trữ nó ở file .json trong thư mục keys hoặc trong root.
+		+ Dùng private key để ký token jwt.
+		+ API dùng public key từ Discovery Endpoint (./well-known/openid-configuration/jwks) để kiểm tra chữ ký.
+	=> is-signing-key...json: Đây là private key được IdentityServer sinh ra để ký token.
+	=> tempkey.jwk: Có thể là phiên bản JWK (Json Web Key) của key đó (Không quan trọng trong dev)
